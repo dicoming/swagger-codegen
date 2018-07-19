312909d325a49277cb277974aee3db28822e207b
usmanovbf <usmanovbf@gmail.com>
2018-07-19 16:56:02 +0300

    todo, @EqualsAndHashCode(callSuper = true)

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SpringDicomCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SpringDicomCodegen.java
@@ -3,6 +3,9 @@ package io.swagger.codegen.languages;
 /**
  * SpringCodegen class for generating DICOM Consulting company's APIs
  */
+// TODO: 10.07.18 подтягивать LocalDateTime из других спек
+// TODO: 10.07.18 подставлять Ideaable, если среди переменных есть idда
+// TODO: 13.07.18 подмечать как @EqualsAndHashcode(callSuper=true), если есть наследование
 public class SpringDicomCodegen extends SpringCodegen {
 
     public SpringDicomCodegen() {
